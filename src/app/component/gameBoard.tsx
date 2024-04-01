c2428e7b553893857ae048e25409bedebf171457
wayne <wayne20180324@gmail.com>
2024-04-01 11:43:11 +0400

    update css

--- src/app/component/gameBoard.tsx
+++ src/app/component/gameBoard.tsx
@@ -7,26 +7,27 @@ interface Props {
 }
 
 const GameBoard: React.FC<Props> = ({ guessingNum, statu }) => {
-    const [textColor, setTextColor] = useState<string>('text-white-600');
+    const [textColor, setTextColor] = useState<string>('text-white');
     const [isInitialRender, setIsInitialRender] = useState<boolean>(true);
 
     useEffect(() => {
         if (isInitialRender) {
-            setTextColor('text-white-600');
+            setTextColor('text-white');
             setIsInitialRender(false);
             return;
         }
         if (statu) {
-            setTextColor('text-white-600');
+            setTextColor('text-white');
         } else {
             setTextColor('text-orange-600');
+            console.log('2', statu, textColor);
         }
     }, [statu]);
 
 
     return (
         <div className="relative h-full">
-            <div className={`absolute left-1/3 bottom-2/4 font-bold text-white text-7xl ${textColor}`}>{guessingNum.toFixed(2)}<span className="text-4xl">x</span></div>
+            <div className={`absolute left-1/3 bottom-2/4 font-bold text-7xl ${textColor}`}>{guessingNum.toFixed(2)}<span className="text-4xl">x</span></div>
             <div className="h-full  pb-10" >
                 <LineGraph guessNumber={guessingNum} />
             </div>
